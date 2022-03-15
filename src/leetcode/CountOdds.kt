710764b13784362c48ed7e3f467653b1d939f306
Parmesh <parmeshtoyou@gmail.com>
2022-03-15 21:55:11 +0530

    Day1: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/submissions/

--- src/leetcode/CountOdds.kt
+++ src/leetcode/CountOdds.kt
@@ -2,16 +2,27 @@ package leetcode
 
 //https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/submissions/
 class CountOdds {
-    fun countOdds(low: Int, high: Int): Int {
+    // Approach 1: Time Complexity is N where N is the number of Items withing the range
+    fun countOdds1(low: Int, high: Int): Int {
         var count = 0
         for(i in low .. high) {
             if(i%2 != 0) count++
         }
         return count
     }
+
+    fun countOdds2(low: Int, high: Int): Int {
+        if (low % 2 == 0 && high % 2 == 0) {
+            return (high - low) / 2
+        } else {
+            return (high - low) / 2 + 1
+        }
+    }
 }
 
 fun main() {
-    val result = CountOdds().countOdds(3, 7) //min 0, max=10^9
-    println(result)
+    val result1 = CountOdds().countOdds1(3, 7) //min 0, max=10^9
+    val result2 = CountOdds().countOdds2(1, 9) //min 0, max=10^9
+    println(result1)
+    println(result2)
 }
