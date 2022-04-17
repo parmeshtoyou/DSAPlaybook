5f18ae6f46323ed82ba23a9ae56963701e43ae7c
Parmesh <parmeshtoyou@gmail.com>
2022-04-17 22:39:28 +0530

    https://leetcode.com/problems/number-of-1-bits/

---
+++ src/main/kotlin/leetcode/NumberOf1sBit.kt
@@ -0,0 +1,21 @@
+package leetcode
+
+fun hammingWeight(n: Int): Int {
+
+    if (n == 0) return 0
+
+    var num = n
+    var count = 0
+    while (num > 0) {
+        if (num % 2 == 0) {
+            num = num.shr(1)
+        } else {
+            count++
+            num = num.shr(1)
+        }
+    }
+
+    println(count)
+
+    return count
+}
---
+++ src/test/kotlin/leetcode/NumberOf1sBitTest.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NumberOf1sBitTest {
+
+    @Test
+    fun hammingWeight() {
+        val result = hammingWeight(15)
+        assertEquals(4, result)
+    }
+}
